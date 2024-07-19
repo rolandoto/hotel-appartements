e0e559da5f71fe2efbbf5c3112fbc5f0572a38e8
Jhonathan Velez <kokoa.arte@gmail.com>
2024-07-19 15:31:59 -0500

    intiail commit updte

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -19,27 +19,17 @@ const Search =({contextShowMenuPeople,
                     <StyleTitleHotel>Adultos</StyleTitleHotel>
                     <StyleGroup>
                     <StyledMenuItemNumber>
-                      <StyleSpan onClick={handDecreaseAdults} > <CiCircleMinus color="#f97316"  fontSize={50} /> </StyleSpan>
+                      <StyleSpan onClick={handDecreaseAdults} > <CiCircleMinus color="#22c55e"  fontSize={50} /> </StyleSpan>
                         <StyleNumberPeople maxlength="30" disabled={true} value={adults} minLength={2} min="0" placeholder="0" />
-                        <StyleSpan onClick={handChangeAdults}  > <CiCirclePlus color="#f97316" fontSize={50} /> </StyleSpan>
+                        <StyleSpan onClick={handChangeAdults}  > <CiCirclePlus color="#22c55e" fontSize={50} /> </StyleSpan>
                         </StyledMenuItemNumber>
                     </StyleGroup>
                   </StyledMenuItem>
 
-                  <StyledMenuItem>
-                    <StyleTitleHotel>Niños</StyleTitleHotel>
-                    <StyleGroup>
-                    <StyledMenuItemNumber>
-                      <StyleSpan  onClick={handDecreaseChildren} > <CiCircleMinus color="#f97316"  fontSize={50} /> </StyleSpan>
-                        <StyleNumberPeople  value={childrem}  disabled={true}  maxlength="30" minLength={2} min="0"  placeholder="0" />
-                        <StyleSpan onClick={handChangeChildrem} > <CiCirclePlus color="#f97316" fontSize={50} /> </StyleSpan>
-                        </StyledMenuItemNumber>
-                    </StyleGroup>
-                  </StyledMenuItem>
                   <StyledMenuItemButton>
                       <button
                           onClick={() => setContextShowMenuPeople(false)}
-                          className=" Button-Search w-[150px] bg-orange-500 text-white py-2  rounded hover:bg-orange-600 transition duration-200">
+                          className=" Button-Search w-[150px] bg-green-500 text-white py-2  rounded hover:bg-green-600 transition duration-200">
                           Buscar
                       </button>
                   </StyledMenuItemButton>   
