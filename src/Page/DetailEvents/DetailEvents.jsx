b005bd15de755b835c50ef7b2386907ae9a059f7
Jhonathan Velez <kokoa.arte@gmail.com>
2024-09-13 11:04:48 -0500

    intial commit

--- src/Page/DetailEvents/DetailEvents.jsx
+++ src/Page/DetailEvents/DetailEvents.jsx
@@ -61,13 +61,9 @@ const DetailEvents =() =>{
         </div>
     </>
     }
-
-
-    
-
     return (<>
                <Header/>
-               <div className="relative bg-cover bg-center h-[410px]" style={{ backgroundImage: `url(https://github.com/rolandoto/image-pms/blob/main/_MG_4223.jpg?raw=true)`,}}>
+               <div className="relative bg-cover bg-center h-[410px]" style={{ backgroundImage: `url(https://github.com/rolandoto/image-pms/blob/main/Piscina.jpg?raw=true)`,}}>
                     <div className="absolute inset-0 bg-black opacity-50"></div>
                     <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
                         <h1 className="text-4xl md:text-6xl lg:text-6xl font-lora">
